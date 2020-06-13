f387ca3ee28c8083b56ef333df8be9a8c42d355a
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2020-06-13 18:31:00 -0400

    Add editor modes and debugging mode

--- src/editor.py
+++ src/editor.py
@@ -12,12 +12,15 @@ class Constants:
     BAR_COLOR = (2, 7, 237)
     DIM_COLOR = (3, 221, 237)
     FILE_COLOR = (4, 200, 237)
-    LAST_COMMAND_COLOR = (5, 35, 237)
+    MODE_COLOR = (5, 35, 237)
+    MODE_INSERT = 'INSERT'
+    MODE_COMMAND = 'COMMAND'
 
 class Editor:
     def __init__(self, stdscr, args):
         self.stdscr = stdscr
         self.args = args
+        self.debug_mode = args.debug
         # setup colors
         start_color()
         use_default_colors()
@@ -25,7 +28,7 @@ class Editor:
         init_pair(*Constants.BAR_COLOR)
         init_pair(*Constants.DIM_COLOR)
         init_pair(*Constants.FILE_COLOR)
-        init_pair(*Constants.LAST_COMMAND_COLOR)
+        init_pair(*Constants.MODE_COLOR)
         # set initial values
         self.height, self.width = stdscr.getmaxyx()
         self.scrtop, self.scrbottom = 0, self.height - 2 # exclusive
@@ -33,7 +36,7 @@ class Editor:
         self.caret_y, self.caret_x = 0, 0
         self.lines = ['']
         self.file_name = 'None'
-        self.cur_command = None
+        self.mode = Constants.MODE_COMMAND
         # try to find a file
         try:
             if args.path is not None:
@@ -44,26 +47,21 @@ class Editor:
             print('The path given is invalid.')
             os._exit(1)
         except UnicodeDecodeError:
-            # try to open the file in binary format
-            try:
-                with open(args.path, 'rb') as edit_file:
-                    self.lines = [str(edit_file.read())[2 : -1]]
-                self.file_name = os.path.basename(args.path)
-            except Exception:
-                print('The encoding of the file is not supported.')
-                os._exit(1)
+            print('The encoding of the file is not supported.')
+            os._exit(1)
 
     def display_text(self):
+        self.height, self.width = self.stdscr.getmaxyx()
         self.stdscr.erase()
         self.stdscr.addstr(' Dim v1', color_pair(3))
         self.stdscr.addstr(' ' * 5, color_pair(2))
         self.stdscr.addstr('Editing ' + self.file_name, color_pair(4))
-        if self.cur_command is not None:
-            self.stdscr.addstr(' ' * (self.width - 30 - len(self.file_name) - len(self.cur_command)), color_pair(2))
-            self.stdscr.addstr(self.cur_command, color_pair(5))
-            self.stdscr.addstr(' ' * 10, color_pair(2))
-        else:
-            self.stdscr.addstr(' ' * (self.width - 20 - len(self.file_name)), color_pair(2))
+        self.stdscr.addstr(
+            ' ' * (self.width - 36 - len(self.file_name) - len(self.mode)),
+            color_pair(2)
+        )
+        self.stdscr.addstr('Mode: ' + self.mode, color_pair(5))
+        self.stdscr.addstr(' ' * 10, color_pair(2))
         self.stdscr.addstr('─' * self.width, color_pair(2))
         if not self.lines:
             self.stdscr.addstr('')
@@ -145,86 +143,147 @@ class Editor:
             self.caret_y = len(self.lines) - 1
         self.caret_x = min(len(self.lines[self.caret_y]), self.caret_x)
 
-    def main(self):
+    def launch(self):
         self.stdscr.bkgd(' ', color_pair(1) | A_BOLD)
+        if self.debug_mode:
+            self.stdscr.addstr(' You have launched the editor in debug mode...')
+            self.stdscr.getkey()
         self.display_text()
         while True:
             key = self.stdscr.getkey()
             self.height, self.width = self.stdscr.getmaxyx()
-            if key == chr(27):
-                # escape
-                break
-            elif key == '\b' or key == 'KEY_BACKSPACE':
-                # backspace
-                if self.caret_x != 0:
-                    line = self.lines[self.caret_y]
-                    line = line[ : self.caret_x - 1] + line[self.caret_x : ]
-                    self.lines[self.caret_y] = line
-                    self.caret_x -= 1
-                elif self.caret_y != 0:
-                    line = self.lines[self.caret_y]
-                    self.lines.pop(self.caret_y)
-                    self.caret_y -= 1
-                    self.caret_x = len(self.lines[self.caret_y])
-                    self.lines[self.caret_y] += line
-            elif key == '\t':
-                # tab
-                line = self.lines[self.caret_y]
-                line = line[ : self.caret_x] + (' ' * 4) + line[self.caret_x : ]
-                self.lines[self.caret_y] = line
-                self.caret_x += 4
-            elif key == '\n' or key == chr(13):
-                # newline or carriage return
-                if self.caret_x == len(self.lines[self.caret_y]):
-                    self.lines.insert(self.caret_y + 1, '')
-                else:
-                    dif = self.lines[self.caret_y][self.caret_x : ]
-                    self.lines[self.caret_y] = self.lines[self.caret_y][ : self.caret_x]
-                    self.lines.insert(self.caret_y + 1, dif)
-                self.caret_x = 0
-                self.caret_y += 1
-            elif key == 'KEY_RESIZE':
-                pass
-            elif key == 'KEY_LEFT':
-                self.move_left(1)
-            elif key == 'KEY_RIGHT':
-                self.move_right(1)
-            elif key == 'KEY_UP':
-                self.move_up(1)
-            elif key == 'KEY_DOWN':
-                self.move_down(1)
-            elif key == 'KEY_PPAGE':
-                # page up
-                self.caret_y = 0
-                self.caret_x = 0
-            elif key == 'KEY_NPAGE':
-                # page down
-                self.caret_y = len(self.lines) - 1
-                self.caret_x = len(self.lines[self.caret_y])
-            elif key == 'KEY_HOME':
-                # go to left
-                self.caret_x = 0
-            elif key == 'KEY_END':
-                # go to right
-                self.caret_x = len(self.lines[self.caret_y])
-            elif key.startswith('ALT'):
-                # alt + key
-                self.cur_command = key
-            elif key in Constants.ALLOWED_CHARS:
-                # allowed text characters
-                line = self.lines[self.caret_y]
-                line = line[ : self.caret_x] + key + line[self.caret_x : ]
-                self.lines[self.caret_y] = line
-                self.caret_x += 1
+            if self.mode == Constants.MODE_COMMAND:
+                self.parse_command(key)
+            else:
+                self.parse_insert(key)
             self.display_text()
+
+    def parse_command(self, key):
+        if key == chr(27):
+            # escape
+            os._exit(1)
+        elif key == 'i':
+            self.mode = Constants.MODE_INSERT
+        elif key == 's':
+            if self.debug_mode:
+                self.stdscr.erase()
+                self.stdscr.addstr(' Confirm that you want to save the file (type \'save\'): ')
+                self.stdscr.refresh()
+                for desired_key in ['s', 'a', 'v', 'e']:
+                    cur_key = self.stdscr.getkey()
+                    self.stdscr.addstr(cur_key)
+                    self.stdscr.refresh()
+                    if cur_key != desired_key:
+                        return
+            try:
+                if self.args.path is not None:
+                    if not os.path.isfile(self.args.path): 
+                        raise FileNotFoundError
+                    with open(self.args.path, 'w') as edit_file:
+                        edit_file.write('\n'.join(self.lines))
+            except (FileNotFoundError, PermissionError, OSError):
+                print('The current file can no longer be found.')
+                os._exit(1)
+            except UnicodeDecodeError:
+                print('The encoding of the file is not supported.')
+                os._exit(1)
+        elif key == 'KEY_RESIZE':
+            pass
+        elif key == 'KEY_LEFT':
+            self.move_left(1)
+        elif key == 'KEY_RIGHT':
+            self.move_right(1)
+        elif key == 'KEY_UP':
+            self.move_up(1)
+        elif key == 'KEY_DOWN':
+            self.move_down(1)
+        elif key == 'KEY_PPAGE':
+            # page up
+            self.caret_y = 0
+            self.caret_x = 0
+        elif key == 'KEY_NPAGE':
+            # page down
+            self.caret_y = len(self.lines) - 1
+            self.caret_x = len(self.lines[self.caret_y])
+        elif key == 'KEY_HOME':
+            # go to left
+            self.caret_x = 0
+        elif key == 'KEY_END':
+            # go to right
+            self.caret_x = len(self.lines[self.caret_y])
+
+    def parse_insert(self, key):
+        if key == chr(27):
+            # escape
+            self.mode = Constants.MODE_COMMAND
+        elif key == '\b' or key == 'KEY_BACKSPACE':
+            # backspace
+            if self.caret_x != 0:
+                line = self.lines[self.caret_y]
+                line = line[ : self.caret_x - 1] + line[self.caret_x : ]
+                self.lines[self.caret_y] = line
+                self.caret_x -= 1
+            elif self.caret_y != 0:
+                line = self.lines[self.caret_y]
+                self.lines.pop(self.caret_y)
+                self.caret_y -= 1
+                self.caret_x = len(self.lines[self.caret_y])
+                self.lines[self.caret_y] += line
+        elif key == '\t':
+            # tab
+            line = self.lines[self.caret_y]
+            line = line[ : self.caret_x] + (' ' * 4) + line[self.caret_x : ]
+            self.lines[self.caret_y] = line
+            self.caret_x += 4
+        elif key == '\n' or key == chr(13):
+            # newline or carriage return
+            if self.caret_x == len(self.lines[self.caret_y]):
+                self.lines.insert(self.caret_y + 1, '')
+            else:
+                dif = self.lines[self.caret_y][self.caret_x : ]
+                self.lines[self.caret_y] = self.lines[self.caret_y][ : self.caret_x]
+                self.lines.insert(self.caret_y + 1, dif)
+            self.caret_x = 0
+            self.caret_y += 1
+        elif key == 'KEY_RESIZE':
+            pass
+        elif key == 'KEY_LEFT':
+            self.move_left(1)
+        elif key == 'KEY_RIGHT':
+            self.move_right(1)
+        elif key == 'KEY_UP':
+            self.move_up(1)
+        elif key == 'KEY_DOWN':
+            self.move_down(1)
+        elif key == 'KEY_PPAGE':
+            # page up
+            self.caret_y = 0
+            self.caret_x = 0
+        elif key == 'KEY_NPAGE':
+            # page down
+            self.caret_y = len(self.lines) - 1
+            self.caret_x = len(self.lines[self.caret_y])
+        elif key == 'KEY_HOME':
+            # go to left
+            self.caret_x = 0
+        elif key == 'KEY_END':
+            # go to right
+            self.caret_x = len(self.lines[self.caret_y])
+        elif key in Constants.ALLOWED_CHARS:
+            # allowed text characters
+            line = self.lines[self.caret_y]
+            line = line[ : self.caret_x] + key + line[self.caret_x : ]
+            self.lines[self.caret_y] = line
+            self.caret_x += 1 
 
 def main(stdscr):
     parser = argparse.ArgumentParser()
     parser.add_argument('-path', '-p', help='path to file being edited')
+    parser.add_argument('-debug', help='launch the editor in debug mode', action='store_true')
     args = parser.parse_args()
     try:
         editor = Editor(stdscr, args)
-        editor.main()
+        editor.launch()
     except:
         print(traceback.format_exc())
 
